f61828c4998ff171ea4c820bd8e7a0dc65074eb0
Edgar García <cristobal_garcia@comunidad.unam.mx>
2023-03-08 10:37:37 -0600

    Agregando comentarios

--- RC4.py
+++ RC4.py
@@ -1,16 +1,19 @@
 import fileinput
 from pydoc import plain
 
+
+#Función que recibe las valores de entrada del programa para ejecutar los test cases.
 def reveiveInput():
     lines = []
     newLines = []
     for line in fileinput.input():
         lines.append(line)
+    #Quitando los salto de linea de las entradas
     for sub in lines:
         newLines.append(sub.replace("\n", ""))
     return newLines
 
-
+#Función que realiza el algoritmo KSA para realizar una permUtación de valores de una matriz S.
 def KSA(key):
     S = []
     keyLength = len(key)
@@ -27,7 +30,7 @@ def KSA(key):
         S[j] = flag
     return S
 
-
+#Función que realiza el algorirmo PRGA, el cual genera el keystream para cifrar el texto plano
 def PRGA(S, textLenght):
     i, j, n = [0, 0, 0]
     keyStream = []
@@ -42,24 +45,41 @@ def PRGA(S, textLenght):
         n += 1
     return keyStream
 
+#Función main del programa
 if __name__ == "__main__":
+    #Iniciando listas convenientes para la ejecución del programa
     cypherText = []
     keyStreamHex = []
     cypherTextDec = []
     cypherTextHex = []
+
+    #Recibiendo entradas de Alphagrader (la key y el texto plano)
     lines = reveiveInput()
     key = lines[0]
-    S = KSA(key)
     plainText = lines[1]
+
+    #Obteniendo matriz S
+    S = KSA(key)
+
+    #Obteniendo keystream con valor expresado en decimal.
     keyStreamDec = PRGA (S, len(plainText))
+
+    #En este ciclo se obtiene el valor del keystream en hexadecimal y en mayusculas.
     for i in range(0, len(keyStreamDec)):
         keyStreamHex.append(format(keyStreamDec[i], 'x'))
         keyStream = ''.join(keyStreamHex).upper()
+    
+    #Obteniendo valor en ascii del texto plano
     plainText = [ord(c) for c in plainText]
     
+    #Cifrando texto plano haciendo operaciones XOR
     for i in range(0, len(plainText)):
         cypherTextDec.append(plainText[i] ^ keyStreamDec[i])
+
+    #Expresando el texto cifrado en hexadecimal
     for i in range(0, len(cypherTextDec)):
         cypherTextHex.append(hex(cypherTextDec[i])[2:].zfill(2))
         cypherText = ''.join(cypherTextHex).upper()
+    
+    #Imprimiendo texto cifrado final
     print(cypherText)
