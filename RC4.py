4503d32c5d68e4ff33a7754943be134ffbe5cf86
Edgar García <cristobal_garcia@comunidad.unam.mx>
2023-03-08 01:35:12 -0600

    Primera prueba RC4

---
+++ RC4.py
@@ -0,0 +1,61 @@
+import fileinput
+from pydoc import plain
+
+def reveiveInput():
+    lines = []
+    for line in fileinput.input():
+        lines.append(line)
+    return lines
+
+
+def KSA(key):
+    S = []
+    keyLength = len(key)
+    #Iniciando arreglo de bytes
+    for i in range(0, 256):
+        S.append(i)
+    #Iniciando permutaciones del vector S
+    j = 0
+    for i in range(0, 256):
+        asciiValue = [ord(c) for c in key[i % keyLength]]
+        j = (j + S[i] + asciiValue[0]) % 256
+        flag = S[i]
+        S[i] = S[j]
+        S[j] = flag
+    return S
+
+
+def PRGA(S, textLenght):
+    i, j, n = [0, 0, 0]
+    keyStream = []
+    while (n < textLenght):
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        flag = S[i]
+        S[i] = S[j]
+        S[j] = flag
+        k = S[(S[i] + S[j]) % 256]
+        keyStream.append(k)
+        n += 1
+    return keyStream
+
+if __name__ == "__main__":
+    cypherText = []
+    keyStreamHex = []
+    cypherTextDec = []
+    cypherTextHex = []
+    lines = reveiveInput()
+    S = KSA(lines[0])
+    plainText = 'lines[0]'
+    keyStreamDec = PRGA (S, len(plainText))
+    for i in range(0, len(keyStreamDec)):
+        keyStreamHex.append(format(keyStreamDec[i], 'x'))
+        keyStream = ''.join(keyStreamHex).upper()
+    plainText = [ord(c) for c in plainText]
+    
+    for i in range(0, len(plainText)):
+        cypherTextDec.append(plainText[i] ^ keyStreamDec[i])
+    for i in range(0, len(cypherTextDec)):
+        cypherTextHex.append(hex(cypherTextDec[i])[2:].zfill(2))
+        cypherText = ''.join(cypherTextHex).upper()
+    print(cypherText)
